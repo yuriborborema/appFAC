921384a829421c73ea0e771620c874cb4de0074e
yuriborborema <yuriborborema10@gmail.com>
2019-01-22 21:33:41 -0200

    w/ rn-map

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MapView style={styles.map}
+          region={{
+            latitude:-16.700380,
+            longitude: -49.242091,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1
+          }}
+          >
+
+          <MapView.Marker
+              coordinate={{
+                latitude:-16.700380,
+                longitude: -49.242091,
+              }}
+              title={'Home'}
+              description={'Yuris home'}
+          />
+
+          </MapView>
       </View>
     );
   }
@@ -13,9 +32,19 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      position: 'absolute',
+      top:0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      justifyContent: 'flex-end',
+      alignItems: 'center'
   },
+  map: {
+    position: 'absolute',
+    top:80,
+    left: 10,
+    bottom: 200,
+    right: 10
+  }
 });
