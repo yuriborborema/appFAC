8a6a3437552b68054d67ca415daac4bd807a1d61
yuriborborema <yuriborborema10@gmail.com>
2019-02-20 21:57:33 -0300

    dada

--- App.js
+++ App.js
@@ -1,28 +1,59 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import {Permissions, Location} from 'expo';
 
 export default class App extends React.Component {
+
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      region: null,
+    }
+
+    this._getLocationAsync();
+
+  }
+
+
+  _getLocationAsync = async () => {
+
+    let {status} = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted'){
+        //Alert.alert('Por favor, permita a utilização de sua Localização');
+        console.log('no permission');
+      }
+
+      let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+      let region = {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+      }
+
+      this.setState({region: region});
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <MapView style={styles.map}
-          region={{
-            latitude:-16.700380,
-            longitude: -49.242091,
-            latitudeDelta: 0.1,
-            longitudeDelta: 0.1
-          }}
+          initialRegion={this.state.region}
+          showsUserLocation={true}
           >
 
-          <MapView.Marker
+          {<MapView.Marker
               coordinate={{
                 latitude:-16.700380,
                 longitude: -49.242091,
               }}
               title={'Home'}
               description={'Yuris home'}
-          />
+          />}
 
           </MapView>
       </View>
