0f216a998f23a874a3af1f7e6da6f9025e6a9cbb
yuriborborema <yuriborborema10@gmail.com>
2019-03-05 20:42:45 -0300

    btt plus

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {Permissions, Location} from 'expo';
 import { TextInput } from 'react-native-gesture-handler';
 import { Ionicons } from '@expo/vector-icons';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {CreateStackNavigator} from 'react-navigation';
 
 export default class App extends React.Component {
 
@@ -19,6 +20,10 @@ export default class App extends React.Component {
     this._getLocationAsync();
 
   }
+
+  jsonMapa = [
+    
+  ]
 
 
   _getLocationAsync = async () => {
@@ -51,22 +56,69 @@ export default class App extends React.Component {
         <MapView style={styles.map}
           initialRegion={this.state.region}
           showsUserLocation={true}
+          customMapStyle={              //tira landmarks do mapa
+          [
+            {
+              "featureType": "poi",
+              "elementType": "labels.text",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.business",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "road",
+              "elementType": "labels.icon",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "transit",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            }
+          ]}
+
           >
 
-          { <MapView.Marker
+           <MapView.Marker
               coordinate={{
                 latitude:-16.700380,
                 longitude: -49.242091,
               }}
-              title={'Home'}
-              description={'Yuris  mhome'}
-          />}
+              title={'Home '}
+              description={'Comunidade  Santa Clara'}
+              image={require('./images/church.png')}
+              
+          />
 
           </MapView>
+
           <View style={styles.textInputCont}>
-          <Ionicons name="md-search" size={25} style={{transform: [{ rotate: '90deg'}]}} />
-          <TextInput placeholder="Procure e acharás!"/>
-          <Icon name="filter" size={25}  />
+          <Ionicons name="md-search" size={28} style={{transform: [{ rotate: '90deg'}]}} color="#ffe047" />
+          <TextInput placeholder="Procure e acharás !"/>
+          <Icon name="filter" size={28} color="#ffe047" />
+
+          </View>
+
+          <View style={styles.addChurch}>
+          <Icon name="plus-circle" size={65} color="#ffe047" />
+
           </View>
       </View>
       //</View>
@@ -105,8 +157,20 @@ const styles = StyleSheet.create({
      shadowRadius: 5,
      shadowOpacity: 1.0,
      borderRadius: 20,
+  },
+  addChurch:{
+    position: 'absolute',
+    bottom: 85,
+    right:35,
+    backgroundColor: 'white',
+    shadowColor: '#000000' ,
+    width: 100,
+    height: 100,
+    borderRadius: 100/2,
     
+    elevation: 7,
+     shadowRadius: 10,
+     shadowOpacity: 1.1,
     
-
   }
 });
